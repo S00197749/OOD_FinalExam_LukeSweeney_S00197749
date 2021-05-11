024328c9330e74df54e0e8fa4e3ee23d33059f0f
Luke Sweeney <60261891+lsweeney00@users.noreply.github.com>
2021-05-11 09:43:07 +0100

    Added Game class

---
+++ LukeSweeney_S00197749/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukeSweeney_S00197749
+{
+    class Game
+    {
+        public string Name { get; set; }
+        public double CriticScore { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public decimal Price { get; set; }
+        public string GameImage { get; set; }
+
+        public Game(string name, string platform, double score, decimal price, string image, string description)
+        {
+            Name = name;
+            CriticScore = score;
+            Description = description;
+            Platform = platform;
+            Price = price;
+            GameImage = image;
+        }
+    }
+}
