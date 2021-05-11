363ad4d237c9c695532a54a29119ba728136abb4
Luke Sweeney <60261891+lsweeney00@users.noreply.github.com>
2021-05-11 11:07:47 +0100

    Added filter for platforms

--- LukeSweeney_S00197749/MainWindow.xaml.cs
+++ LukeSweeney_S00197749/MainWindow.xaml.cs
@@ -19,7 +19,11 @@ namespace LukeSweeney_S00197749
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {   
+    {
+        //create lists
+        List<Game> filteredGames = new List<Game>();
+        List<Game> allGames = new List<Game>();
+
         //link to database
         GameData db = new GameData();
         public MainWindow()
@@ -28,15 +32,25 @@ namespace LukeSweeney_S00197749
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //get all games in db
             var query = from g in db.Games
                         select g;
 
-            lbxGames.ItemsSource = query.ToList();
+            //put all games into allGames list and display in listbox
+            allGames = query.ToList();
+            lbxGames.ItemsSource = allGames;
+
+            //create list of all platforms and display in combo box
+            List<string> allPlatforms = new List<string> {"All", "PC", "PS", "Xbox", "Switch"};
+            cbxPlatform.ItemsSource = allPlatforms;
+            cbxPlatform.SelectedIndex = 0;
         }
         private void lbxGames_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            //get selected game
             Game selectedGame = lbxGames.SelectedItem as Game;
 
+            //if a game is selected, show details
             if(selectedGame != null)
             {
                 tblkGameDetails.Text = $"{selectedGame.Name}\nPlatform: {selectedGame.Platform}\n{selectedGame.Price:C}";
@@ -44,5 +58,29 @@ namespace LukeSweeney_S00197749
             }
         }
 
+        private void cbxPlatform_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //get selected platform 
+            string selectedPlatform = cbxPlatform.SelectedItem.ToString();
+
+            //switch statement to determine which games to display
+            switch (selectedPlatform)
+            {
+                case "All":
+                    lbxGames.ItemsSource = allGames;
+                    break;
+                case "PC":
+                case "PS":
+                case "Xbox":
+                case "Switch":
+                    var query = from g in db.Games
+                                where g.Platform.Contains(selectedPlatform)
+                                select g;
+                    filteredGames = query.ToList();
+                    break;
+            }
+            if(selectedPlatform != "All")
+                lbxGames.ItemsSource = filteredGames;
+        }
     }
 }
