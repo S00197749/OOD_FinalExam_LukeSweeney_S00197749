2b4ced866c01509320d5366fd8ce3788f63ef7ed
Luke Sweeney <60261891+lsweeney00@users.noreply.github.com>
2021-05-11 09:34:41 +0100

    Add project files.

---
+++ LukeSweeney_S00197749/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LukeSweeney_S00197749
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
